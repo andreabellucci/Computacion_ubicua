7253fd360ebfc6633160ab0d4e69be86e4acd670
Big-N0thing <Noefuente12@hotmail.com>
2021-10-04 16:46:52 +0200

    final final final final mamahuevo

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const mymap = L.map('sample_map').setView([40.416729, -3.703339], 10);
 let flagIcon = L.Icon.extend({
   options: {
     iconUrl: 'https://img.icons8.com/cotton/2x/000000/finish-flag.png',
+    shadowUrl: 'https://img1.picmix.com/output/stamp/normal/5/9/2/9/469295_ad0b5.png',
     iconSize: 47,
-    shadowSize: [50, 64],
+    shadowSize: 40,
     iconAnchor: [20, 46],
+    shadowAnchor: [21, 23],
     popupAnchor: [-3, -76]
   }
 });
@@ -22,6 +24,7 @@ let start = false;
 // Keep track with the user and check if he's close to destination
 setInterval(updatePosition, 1000);
 setInterval(checkDistance, 1000);
+setInterval(calculateDistance, 1000);
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -63,4 +66,27 @@ function checkDistance() {
     } else
       navigator.vibrate(0); // To cancel the vibration if we change the marker
   }
+}
+
+function calculateDistance() {  // generally used geo measurement function
+  if (start) {
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = myPosition._latlng.lat * Math.PI / 180 - myDestination._latlng.lat * Math.PI / 180;
+    var dLon = myPosition._latlng.lng * Math.PI / 180 - myDestination._latlng.lng * Math.PI / 180;
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(myDestination._latlng.lat * Math.PI / 180) * Math.cos(myPosition._latlng.lat * Math.PI / 180) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    var result = d * 1000; // meters
+    document.getElementById("distance_indicator").innerText = Math.round(result) + " meters";
+
+  }else{
+    document.getElementById("distance_indicator").innerText = "Waiting for Destination...";
+  }
+}
+
+// Function to center the view on the users position
+function centerView() {
+  mymap.setView(myPosition._latlng, 25);
 }
