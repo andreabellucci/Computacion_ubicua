0f9cf17fa61e965f73ccff4b9bcf11a7f112f9ee
Big-N0thing <Noefuente12@hotmail.com>
2021-11-11 16:13:29 +0100

    Inicio react

--- SPOC_3/client/src/App.js
+++ SPOC_3/client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header id="header_div">
+        <img src="https://logodix.com/logo/1229689.png" alt="messenger butterfly icon" />
+        <div>
+          <p>sendertext</p>
+        </div>
       </header>
+
+      <div id="chat_container"></div>
+
+      <footer id="footer_div">
+        <input type="text" id="input_message" placeholder="message..." />
+        <input type="submit" id="input_submit" value="&#10148;" />
+      </footer>
     </div>
   );
 }
