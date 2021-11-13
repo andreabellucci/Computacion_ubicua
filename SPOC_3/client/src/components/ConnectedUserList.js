04aabe1850d7c535f90ae4350db7f969bb362d22
Big-N0thing <Noefuente12@hotmail.com>
2021-11-13 15:57:17 +0100

    va tirando va tirando

--- SPOC_3/client/src/components/ConnectedUserList.js
+++ SPOC_3/client/src/components/ConnectedUserList.js
@@ -7,7 +7,7 @@ function ConnectedUserList(props) {
       <div id="connected_users">
         {props.usersList.map((val, key) => {
           return (
-            <div key={key} className="connected_user_container">
+            <div key={key} className="connected_user_container" onClick={ }>
               <img
                 src="https://img.utdstc.com/icon/9a8/867/9a8867eb77f8a20e62b5ea69f900de7c650546db544a00ce042d66945fd987bb:200"
                 alt="messenger butterfly icon" />
