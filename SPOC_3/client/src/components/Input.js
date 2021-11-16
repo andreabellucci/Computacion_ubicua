00eb6fef524261d6e8f7f58c6b2277685b31a7c3
Big-N0thing <Noefuente12@hotmail.com>
2021-11-16 12:19:43 +0100

    Avances chulos

--- SPOC_3/client/src/components/Input.js
+++ SPOC_3/client/src/components/Input.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "./Context";
 
 export default function Input() {
@@ -10,26 +10,55 @@ export default function Input() {
   const [currentView, setCurrentView] = value6;
   const [currentPrivateChat, setCurrentPrivateChat] = value7;
   const [newMessage, setNewMessage] = value8;
+  const [timerMessage, setTimerMessage] = useState(null);
+  const [pendingTimerMessage, setPendingTimerMessage] = useState(null);
 
-  function sendPublicMessage() {
+  function sendPublicMessage(mode) {
     // Extract the message from the box and clear it
     if (newMessage !== "") {
       let newGlobalMessage = { from: username, text: newMessage, datetime: new Date().toLocaleString() };
-      socket.emit("broadcast_public_message", newGlobalMessage);
       document.getElementById("input_message").value = "";
       setNewMessage("");
+      setPendingTimerMessage(newGlobalMessage);
+
+      if (mode === "normal") {
+        socket.emit("broadcast_public_message", newGlobalMessage);
+      } else if (mode === "timer") {
+
+        // Send the message in the next 5 seconds...
+        setTimerMessage(setTimeout(() => {
+          socket.emit("broadcast_public_message", newGlobalMessage);
+          setTimerMessage(null);
+        }, 5000));
+      }
     }
   }
 
-  function sendPrivateMessage() {
+  function sendPrivateMessage(mode) {
     // Extract the message from the box and clear it
     if (newMessage !== "") {
       let newPrivateMessage = { from: username, to: currentPrivateChat, text: newMessage, datetime: new Date().toLocaleString() };
-      socket.emit("send_private_message", newPrivateMessage);
-      setPrivateMessageStack((prevMessageStack) => [...prevMessageStack, newPrivateMessage]);
       document.getElementById("input_message").value = "";
       setNewMessage("");
+      setPendingTimerMessage(newPrivateMessage);
+
+      if (mode === "normal") {
+        socket.emit("send_private_message", newPrivateMessage);
+      } else if (mode === "timer") {
+
+        // Send the message in the next 5 seconds...
+        setTimerMessage(setTimeout(() => {
+          socket.emit("send_private_message", newPrivateMessage);
+          setPrivateMessageStack((prevMessageStack) => [...prevMessageStack, newPrivateMessage]);
+          setTimerMessage(null);
+        }, 5000));
+      }
     }
+  }
+
+  function cancelMessage() {
+    clearTimeout(timerMessage);
+    setTimerMessage(null);
   }
 
   function handleOnInput(e) {
@@ -41,11 +70,21 @@ export default function Input() {
       {(currentView === "global" || currentView === "private") &&
         <footer id="footer_div">
           <input onInput={handleOnInput} type="text" id="input_message" placeholder="message..." />
-          {currentView === "global" &&
-            <input type="submit" onClick={sendPublicMessage} id="input_submit" value="&#10148;" />
+          {timerMessage &&
+            <input type="submit" onClick={cancelMessage} class="input_submit" value="&#128473;" />
           }
-          {currentView === "private" &&
-            <input type="submit" onClick={sendPrivateMessage} id="input_submit" value="&#10148;" />
+          {currentView === "global" && !timerMessage &&
+            <input type="submit" onClick={() => sendPublicMessage("timer")} class="input_submit" value="&#128337;" />
+          }
+          {currentView === "private" && !timerMessage &&
+            < input type="submit" onClick={() => sendPrivateMessage("timer")} class="input_submit" value="&#128337;" />
+          }
+
+          {currentView === "global" && !timerMessage &&
+            < input type="submit" onClick={() => sendPublicMessage("normal")} class="input_submit" value="&#10148;" />
+          }
+          {currentView === "private" && !timerMessage &&
+            < input type="submit" onClick={() => sendPrivateMessage("normal")} class="input_submit" value="&#10148;" />
           }
         </footer>
       }
