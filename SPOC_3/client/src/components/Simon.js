7444598f01f2b743040c795476f8399012544346
Big-N0thing <Noefuente12@hotmail.com>
2021-11-20 14:07:06 +0100

    Seguimos con el tito simon

--- SPOC_3/client/src/components/Simon.js
+++ SPOC_3/client/src/components/Simon.js
@@ -5,22 +5,45 @@ export default function Simon() {
 
   const { value6 } = useContext(Context);
   const [currentView, setCurrentView] = value6;
+  const [inGame, setInGame] = useState(false);
+  const [movementSimonStack, setMovementSimonStack] = useState([]);
+  const [userStackIndex, setUserStackIndex] = useState(0);
+
+  function startGame() {
+    setMovementSimonStack([]);
+    setInGame(true);
+  }
+
+  function stopGame() {
+    setInGame(false);
+  }
+
+  function colorChoice(color) {
+    if (inGame) {
+      if (movementSimonStack[userStackIndex] === color) {
+        setUserStackIndex(userStackIndex + 1);
+      } else {
+        alert("Sequence NOT correct, you lose");
+        setInGame(false);
+      }
+    }
+  }
 
   return (
     <div>
       {currentView === "simon" &&
         <div id="simon_game_container">
           <div id="upper_simon">
-            <div id="simon_part_yellow"></div>
-            <div id="simon_part_blue"></div>
+            <div id="simon_part_yellow" onClick={() => colorChoice("yellow")}></div>
+            <div id="simon_part_blue" onClick={() => colorChoice("blue")}></div>
           </div>
           <div id="lower_simon">
-            <div id="simon_part_red"></div>
-            <div id="simon_part_green"></div>
+            <div id="simon_part_red" onClick={() => colorChoice("red")}></div>
+            <div id="simon_part_green" onClick={() => colorChoice("green")}></div>
           </div>
           <div id="simon_game_info">
-            <input type="submit" onClick={() => alert("play")} className="simon_button" value="&#9654;" />
-            <input type="submit" onClick={() => alert("pause")} className="simon_button" value="&#9208;" />
+            <input type="submit" onClick={() => startGame()} className="simon_button" value="&#9654;" />
+            <input type="submit" onClick={() => stopGame()} className="simon_button" value="	&#9209;" />
           </div>
         </div>
       }
