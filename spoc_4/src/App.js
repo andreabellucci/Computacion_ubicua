d18794b913c13f26fff5d358500c02f4dfdba19d
Big-N0thing <Noefuente12@hotmail.com>
2021-12-02 23:49:22 +0100

    A FALTA DE LOS GESTOS

--- spoc_4/src/App.js
+++ spoc_4/src/App.js
@@ -51,6 +51,58 @@ export default function App() {
         };
         setTaskList((oldList) => [...oldList, newAddedTask]);
       });
+
+      // Set up the gesture features once it's logged
+      function configureGestures() {
+        // touch control block
+        let start_x = 0;
+        let end_x = 0;
+        let start_time = 0;
+        let start_hold;
+        const TIME_SLIDE_THRESHOLD = 500; // Timer for Slide action
+        const SPACE_THRESHOLD = 100;
+
+        let taskListContainer = document.getElementById('task_list');
+
+        taskListContainer.addEventListener("touchstart", function (e) {
+          console.log("TOCADO!!!!!");
+          e.preventDefault();
+          start_x = e.targetTouches[0].screenX;
+          start_time = e.timeStamp;
+
+          // if you keep your finger at the task, it will be erased two seconds after
+          start_hold = setTimeout(function () {
+            var target_task = e.changedTouches[0];
+            // extract the index of the selected task
+            var task_index = target_task.target.id.match(/\d+/)[0];
+            doneTask(task_index);
+          }, 2000);
+
+        }, { passive: false });
+
+        taskListContainer.addEventListener("touchmove", function (e) {
+          e.preventDefault();
+          end_x = e.changedTouches[0].screenX;
+        }, { passive: false });
+
+        taskListContainer.addEventListener("touchend", function (e) {
+
+          // clear the timeout that activates hold action
+          clearTimeout(start_hold);
+
+          e.preventDefault();
+          let end_time = e.timeStamp;
+
+          // If this sentence is true, that means you've performed a SLIDE action
+          if (end_time - start_time < TIME_SLIDE_THRESHOLD && end_x - start_x > SPACE_THRESHOLD) {
+            var target_task = e.changedTouches[0];
+            // extract the index of the selected task
+            var task_index = target_task.target.id.match(/\d+/)[0];
+            removeTask(task_index);
+          }
+        });
+      }
+
     }
   }, [isLoggedIn]);
 
@@ -91,11 +143,14 @@ export default function App() {
   }
 
   function handleAddTask() {
-    const taskListRef = ref(db, "tasks/" + uid.current);
-    const newTaskRef = push(taskListRef, {
-      title: inputRef.current,
-      completed: false
-    });
+    if (inputRef.current != "") {
+      const taskListRef = ref(db, "tasks/" + uid.current);
+      const newTaskRef = push(taskListRef, {
+        title: inputRef.current,
+        completed: false
+      });
+    }
+    document.getElementById('input_task').value = "";
   }
 
   function filter() {
@@ -170,14 +225,14 @@ export default function App() {
             {taskList.map((el, i) => {
               if (el.completed) {
                 return (<div className='single_task_container' id={"task_n_" + i} key={i}>
-                  <input type='checkbox' checked className='task_check' id={"task_completed_n_" + i} />
+                  <input type='checkbox' checked={true} className='task_check' id={"task_completed_n_" + i} />
                   <p id={"task_text_n_" + i} className='completed_p'>{el.title}</p>
                   <button onClick={() => doneTask(i)}>done</button>
                   <button onClick={() => removeTask(i)}>delete</button>
                 </div>);
               } else {
                 return (<div className='single_task_container' id={"task_n_" + i} key={i}>
-                  <input type='checkbox' className='task_check' id={"task_completed_n_" + i} />
+                  <input type='checkbox' checked={false} className='task_check' id={"task_completed_n_" + i} />
                   <p id={"task_text_n_" + i} >{el.title}</p>
                   <button onClick={() => doneTask(i)}>done</button>
                   <button onClick={() => removeTask(i)}>delete</button>
